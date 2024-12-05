d54d4de0fef28144f0358327b75dcd5ca1739e60
GOKUL <147173298+GOKUL7387@users.noreply.github.com>
2024-12-05 22:14:35 +0530

    Sort 0s, 1s and 2s.cpp

---
+++ Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,10 @@
+link: https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1
+
+code:
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+    }
+};
