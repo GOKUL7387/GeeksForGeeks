f3885c798e54902c8d1879d7502efa88f95667e4
GOKUL <147173298+GOKUL7387@users.noreply.github.com>
2025-01-03 08:49:41 +0530

    Count Subarrays with given XOR.cpp

---
+++ Count Subarrays with given XOR.cpp
@@ -0,0 +1,22 @@
+link: https://www.geeksforgeeks.org/problems/count-subarray-with-given-xor/1
+
+code:
+
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) 
+    {
+        map<long,long>mp;
+        mp[0]++;
+        long curr = 0;
+        long ans = 0;
+        int n = arr.size();
+        for(int i=0;i<n;i++)
+        {
+            curr^=arr[i];
+            ans += mp[(curr^k)];
+            mp[curr]++;
+        }
+        return ans;
+    }
+};
