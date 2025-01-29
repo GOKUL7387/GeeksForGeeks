8ad49030e765c15adeb6554e11502f6e6e392b4a
GOKUL <147173298+GOKUL7387@users.noreply.github.com>
2025-01-29 20:24:34 +0530

    Implement Pow.cpp

---
+++ Implement Pow.cpp
@@ -0,0 +1,11 @@
+link: https://www.geeksforgeeks.org/problems/powx-n/1
+
+code:
+
+class Solution {
+  public:
+    double power(double b, int e) {
+        double x=pow(b,e);
+        return x;
+    }
+};
